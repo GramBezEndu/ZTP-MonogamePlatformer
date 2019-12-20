762faf72b8bc509285ad4e4ad5d7695f894207bb
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-12-20 01:18:52 +0100

    Add some builder classes

---
+++ ZTP-PlatformerGame/PlatformerEngine/MapsManager/MapReader.cs
@@ -0,0 +1,24 @@
+﻿using PlatformerEngine.Sprites;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PlatformerEngine.MapsManager
+{
+    public class MapReader
+    {
+        private string filePath;
+        /// <summary>
+        /// File should be located in a specific folder (TODO: implement)
+        /// </summary>
+        /// <param name="fileName"></param>
+        public MapReader(string fileName)
+        {
+        }
+        public List<Sprite> BuildMap(MapBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
